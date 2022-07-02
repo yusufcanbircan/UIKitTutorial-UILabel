0579a0f663066fc392c767830652b63138e8de8f
Yusuf Can Bircan <yusufcannnz@hotmail.com>
2022-07-02 16:01:58 +0300

    Initial Commit

---
+++ UIKitTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTutorial
+//
+//  Created by Yusuf Can on 2.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
