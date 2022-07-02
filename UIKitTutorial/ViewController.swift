9bb0a6a052b46126e03c424420fe32e9cbb175a2
Yusuf Can Bircan <yusufcannnz@hotmail.com>
2022-07-02 20:38:03 +0300

    we made username and password controls, if it is wrong, error will appear.

--- UIKitTutorial/ViewController.swift
+++ UIKitTutorial/ViewController.swift
@@ -11,16 +11,33 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        errorLabel.isHidden = true
         
     }
     
     @IBAction func didTapLoginButton(_ sender:UIButton){
-        print("Username : \(String(describing: usernameTextField.text))")
-        print("Password : \(passwordTextField.text ?? "No Input")")
+        
+        if usernameTextField.text == "yusufcanbircan", passwordTextField.isHidden{
+            passwordTextField.isHidden = false
+            errorLabel.isHidden = true
+        } else {
+            if usernameTextField.text != "yusufcanbircan"{
+                errorLabel.text = "Error! Username is wrong!"
+                errorLabel.isHidden = false
+            } else if !(passwordTextField.isHidden), passwordTextField.text == "123456"{
+                activityIndicatorView.startAnimating()
+                errorLabel.isHidden = true
+            } else {
+                errorLabel.text = "Error! Password is wrong!"
+                errorLabel.isHidden = false
+            }
+        }
+        
     }
 
 }
